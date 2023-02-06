14c1ad489663dc3ef0570c7b8c726cef6b12d819
nasperus <gio.melikidze095@gmail.com>
2023-02-06 21:55:13 +0400

    changing files

--- Assets/Scripts/Candy.cs
+++ Assets/Scripts/Candy.cs
@@ -4,15 +4,19 @@ using UnityEngine;
 
 public class Candy : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    
 
-    // Update is called once per frame
-    void Update()
+
+    private void OnTriggerEnter2D(Collider2D collision)
     {
-        
+        if (collision.gameObject.tag == "Player")
+        {
+            Destroy(gameObject);
+        }
+
+        if (collision.gameObject.tag == "Finish")
+        {
+            Destroy(gameObject);
+        }
     }
 }
--- Assets/Scripts/CandySpawner.cs
+++ Assets/Scripts/CandySpawner.cs
@@ -1,18 +1,52 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class CandySpawner : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [SerializeField] GameObject[] candySpawner;
+    [SerializeField] float spawnInterval;
+     private readonly int randPosition = 8;
+
     void Start()
     {
-        
+        StartSpawningCandies();
     }
 
-    // Update is called once per frame
-    void Update()
+  
+ 
+    private void CreateCandies()
     {
+        int randCandy = Random.Range(0,candySpawner.Length);
+        float candyPos = Random.Range(-randPosition, randPosition);
+        Vector3 randPos =  new Vector3(candyPos, transform.position.y, transform.position.z);
+        Instantiate(candySpawner[randCandy], randPos, transform.rotation);
         
+
+    }
+
+    IEnumerator SpawnCandies()
+    {
+       
+        yield return new WaitForSeconds(2f);
+           
+        while (true)
+        {
+            CreateCandies();
+            yield return new WaitForSeconds(spawnInterval);
+        }
+
+    }
+
+
+    public void StartSpawningCandies()
+    {
+        StartCoroutine("SpawnCandies");
+    }
+
+    public void StopSpawningCandies()
+    {
+        StopCoroutine("SpawnCandies");
     }
 }
